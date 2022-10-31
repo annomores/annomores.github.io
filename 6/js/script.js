065c9e3e9dbba4ed93131c08228e6625906fd817
annomores <112850461+annomores@users.noreply.github.com>
2022-10-31 20:53:48 +0300

    Update script.js

--- 6/js/script.js
+++ 6/js/script.js
@@ -16,21 +16,61 @@ function calculator() {
 	return false;
 }
 window.addEventListener('DOMContentLoaded', function (event) {
+
+});
+window.addEventListener('DOMContentLoaded', function (event) {
+	//5
 	console.log("DOM fully loaded and parsed");
 	let button = document.getElementById("final-price");
 	button.addEventListener("click", calculator);
 });
+let carpetCount = document.getElementsByName("carpet_count");
+	carpetCount[0].addEventListener("change", calculator2);
+	let calcRadio = document.querySelector(".calc__radio");
+	let calcCheckbox = document.querySelector(".calc__checkbox");
+
+let calcRadio1 = document.getElementsByName("radio-carpet");
+	calcRadio1[0].addEventListener("change", calculator2);
+	calcRadio1[1].addEventListener("change", calculator2);
+
+	let carpetType = document.getElementsByName("frog_type");
+	let carpetLamp = document.getElementsByName("veselaya_PEPE");
+	veselaya_PEPE[0].addEventListener("change", calculator2);
+	frogType[0].addEventListener("change", function (event) {if (parseInt(frogType[0].value) === 1) {
+			calcRadio.style.display = "none";
+			calcCheckbox.style.display = "none";
+		}
+		else if (parseInt(carpetType[0].value) === 2) {
+			calcRadio.style.display = "none";
+			calcCheckbox.style.display = "flex";
+		}
+		else {
+			calcRadio.style.display = "flex";
+			calcCheckbox.style.display = "none";
+		}
+		veselaya_PEPE[0].checked = false;
+		calcRadio1[0].checked = false;
+		calcRadio1[1].checked = false;
+		calculator2();
+	});
+});
+
+window.addEventListener('DOMContentLoaded', function (event) {
+
+
+});
+
 function calculator2() {
 
-	let carpetCount = document.getElementsByName("carpet_count");
-	let carpetType = document.getElementsByName("carpet_type");
-	let calcRadio1 = document.getElementsByName("radio-carpet");
-	let carpetLamp = document.getElementsByName("carpet_lamp");
-	let carpeResult = document.getElementsByName('carpet_result');
+	let frogCount = document.getElementsByName("frog_count");
+	let frogType = document.getElementsByName("frog_type");
+	let calcRadio1 = document.getElementsByName("radio-frog");
+	let veselaya_PEPE = document.getElementsByName("veselaya_PEPE");
+	let frogResult = document.getElementsByName('frog_result');
 
-	let a = parseInt(carpetCount[0].value, 10);
+	let a = parseInt(frogCount[0].value, 10);
 	if (isNaN(a) || a <= 0) {
-		carpeResult[0].value = "";
+		frogResult[0].value = "";
 		return;
 	}
 
@@ -40,7 +80,7 @@ function calculator2() {
 	if (parseInt(frogType[0].value) === 1) {
 		b = 100;
 	}
-	else if (parseInt(frogType[0].value) === 2) {
+	else if (parseInt(carpetType[0].value) === 2) {
 		b = 450;
 	}
 	else {
@@ -49,7 +89,7 @@ function calculator2() {
 
 
 	if (parseInt(frogType[0].value) === 3 && calcRadio1[0].checked === false && calcRadio1[1].checked === false) {
-		carpeResult[0].value = "";
+		frogResult[0].value = "";
 		return;
 	}
 	if (calcRadio1[0].checked === true) {
@@ -60,9 +100,9 @@ function calculator2() {
 	}
 
 	s = s * b;
-	if (veselayaPEPE[0].checked === true) {
+	if (veselaya_PEPE[0].checked === true) {
 		s += 50;
 	}
 
-	frogResult[0].value = s;
+	carpeResult[0].value = s;
 }

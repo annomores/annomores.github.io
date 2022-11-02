f6612a0c7e2d9adb9d0ea105239389a28aea77d9
annomores <112850461+annomores@users.noreply.github.com>
2022-11-02 16:53:25 +0300

    Update script.js

--- 6/js/script.js
+++ 6/js/script.js
@@ -23,9 +23,8 @@ window.addEventListener('DOMContentLoaded', function (event) {
 	console.log("DOM fully loaded and parsed");
 	let button = document.getElementById("final-price");
 	button.addEventListener("click", calculator);
-
-let carpetCount = document.getElementsByName("frog_count");
-	carpetCount[0].addEventListener("change", calculator2);
+let frogCount = document.getElementsByName("frog_count");
+	frogCount[0].addEventListener("change", calculator2);
 	let calcRadio = document.querySelector(".calc__radio");
 	let calcCheckbox = document.querySelector(".calc__checkbox");
 
@@ -33,9 +32,9 @@ let calcRadio1 = document.getElementsByName("radio-frog");
 	calcRadio1[0].addEventListener("change", calculator2);
 	calcRadio1[1].addEventListener("change", calculator2);
 
-	let carpetType = document.getElementsByName("frog_type");
-	let carpetLamp = document.getElementsByName("veselaya_PEPE");
-	veselaya_PEPE[0].addEventListener("change", calculator2);
+	let frogType = document.getElementsByName("frog_type");
+	let veselayaPEPE = document.getElementsByName("veselaya_PEPE");
+	veselayaPEPE[0].addEventListener("change", calculator2);
 	frogType[0].addEventListener("change", function (event) {if (parseInt(frogType[0].value) === 1) {
 			calcRadio.style.display = "none";
 			calcCheckbox.style.display = "none";
@@ -48,7 +47,7 @@ let calcRadio1 = document.getElementsByName("radio-frog");
 			calcRadio.style.display = "flex";
 			calcCheckbox.style.display = "none";
 		}
-		veselaya_PEPE[0].checked = false;
+		veselayaPEPE[0].checked = false;
 		calcRadio1[0].checked = false;
 		calcRadio1[1].checked = false;
 		calculator2();
@@ -65,7 +64,7 @@ function calculator2() {
 	let frogCount = document.getElementsByName("frog_count");
 	let frogType = document.getElementsByName("frog_type");
 	let calcRadio1 = document.getElementsByName("radio-frog");
-	let veselaya_PEPE = document.getElementsByName("veselaya_PEPE");
+	let veselayaPEPE = document.getElementsByName("veselaya_PEPE");
 	let frogResult = document.getElementsByName('frog_result');
 
 	let a = parseInt(frogCount[0].value, 10);
@@ -100,7 +99,7 @@ function calculator2() {
 	}
 
 	s = s * b;
-	if (veselaya_PEPE[0].checked === true) {
+	if (veselayaPEPE[0].checked === true) {
 		s += 50;
 	}
 
